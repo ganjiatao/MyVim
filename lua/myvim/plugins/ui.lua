f83c5be4bec0af93d45863e7f276938bd394c9ce
LINGGUANG <17671304431@163.com>
2023-09-30 13:08:36 +0800

    2023.9.29修改indent-blankline版本为3.2

--- lua/myvim/plugins/ui.lua
+++ lua/myvim/plugins/ui.lua
@@ -163,28 +163,30 @@ return {
     end,
   },
 
+
   -- indent guides for Neovim
   {
     "lukas-reineke/indent-blankline.nvim",
     event = { "BufReadPost", "BufNewFile" },
     opts = {
-      -- char = "▏",
-      char = "│",
-      filetype_exclude = {
-        "help",
-        "alpha",
-        "dashboard",
-        "neo-tree",
-        "Trouble",
-        "lazy",
-        "mason",
-        "notify",
-        "toggleterm",
-        "lazyterm",
+      indent = { char = "│" },
+      scope = { enabled = false },
+      exclude = {
+        filetypes = {
+          "help",
+          "alpha",
+          "dashboard",
+          "neo-tree",
+          "Trouble",
+          "lazy",
+          "mason",
+          "notify",
+          "toggleterm",
+          "lazyterm",
+        },
       },
-      show_trailing_blankline_indent = false,
-      show_current_context = false,
     },
+    main = "ibl",
   },
 
   -- Active indent guide and indent text objects. When you're browsing
