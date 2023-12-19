e77b1a3fb94b872e2923286c7bf61661033444c2
LINGGUANG <17671304431@163.com>
2023-12-19 11:24:46 +0800

    修改codeium.lua,2023.12.19

--- lua/myvim/my/codeium.lua
+++ lua/myvim/my/codeium.lua
@@ -1,5 +1,12 @@
 return {
-    {
-        'Exafunction/codeium.vim',
+  {
+    "Exafunction/codeium.vim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
     },
+    config = function()
+      require("codeium").setup({})
+    end,
+  },
 }
